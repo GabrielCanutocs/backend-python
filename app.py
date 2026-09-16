fd1abf63ea4b3b731d28f8b0a3bd3c2422443301
GabrielCanutocs <gabrielcanuto2006@gmail.com>
2026-09-15 22:02:54 -0300

    adicionando a funcao de printar equipamentos

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ from modelos.maquinario.maquina_musculacao import MaquinaMusculacao
 academia_movimento = Academia("Movimento", 130)
 supino_reto = MaquinaMusculacao("supino_reto", "titako", "peito", "100kg")
 esteira = MaquinaCardio("esteira", "xlr8", "20km/h")
+academia_movimento.adiciona_equipamento(supino_reto)
+academia_movimento.adiciona_equipamento(esteira)
 
 def main():
-    print(supino_reto)
-    print(esteira)
+    academia_movimento.lista_equipamentos
 
 if __name__ == "__main__":
     main()
--- modelos/academia.py
+++ modelos/academia.py
@@ -1,5 +1,5 @@
 from modelos.avaliacao import Avaliacao
-
+from modelos.maquinario.equipamento import Equipamento
 
 class Academia: 
     academias = []
@@ -8,17 +8,26 @@ class Academia:
         self._nome =  nome.title()
         self._preco = preco
         self._ativo = False
+        self._equipamentos = []
         self._avaliacao = []
         Academia.academias.append(self)
+
 
     def __str__(self):
         return f"{self._nome} | {self._preco}"
 
+
     def ativa_academia(self):
             self._ativo = not self._ativo
 
+
     def receber_avaliacao(self, cliente, nota):
         self._avaliacao.append(Avaliacao(cliente, nota))
+
+
+    def adiciona_equipamento(self, maquina):
+        if isinstance(maquina, Equipamento):
+            self._equipamentos.append(maquina)
 
 
     @classmethod
@@ -35,5 +44,19 @@ class Academia:
     def media_avaliacoes(self):
         if not self._avaliacao:
             return 0
-        
         return round(sum(avaliacao._nota for avaliacao in self._avaliacao) / len(self._avaliacao), 1)
+
+    @property
+    def lista_equipamentos(self):
+        print(f"Equipamentos disponiveis na academia {self._nome}\n")
+        for i,maquina in enumerate(self._equipamentos, start=1):
+
+            if hasattr(maquina, "_velocidade_maxima"):
+                descricao_maquina_cardio = f'{i}. Nome: {maquina._nome} | Marca: {maquina._marca} | Velocidade Maxima: {maquina._velocidade_maxima}'
+                print(descricao_maquina_cardio)
+                
+            else:
+                descricao_maquina_musculacao = f'{i}. Nome: {maquina._nome} | Marca: {maquina._marca} | Grupamento: {maquina._grupamento} | Carga Maxima: {maquina._carga_maxima}'
+                print(descricao_maquina_musculacao)
+
+
